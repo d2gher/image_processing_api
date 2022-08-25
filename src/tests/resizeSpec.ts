e85b76b105e0e02e80f05c92532a30047ea8f0b3
d2gher <d2gher@gmail.com>
2022-08-25 19:49:33 +0200

    added readme file

--- src/tests/resizeSpec.ts
+++ src/tests/resizeSpec.ts
@@ -39,25 +39,3 @@ describe("Function test", () => {
     });
   });
 });
-
-// describe("Functions", () => {
-//   it("Test image processing function", async () => {
-//     let result = 0;
-//     // Creates a new image
-//     await request.get("/resize?name=fjord&width=200&height=600");
-//     // Searches if the image exists
-//     fs.access("./public/images/output/fjord_200_600.jpg", async (err) => {
-//       // if it didn't find it then that means the process failed, result will equal 0
-//       if (err) result = 0;
-//       // if it didn find it then that means the process succeded
-//       // the file will be removed and result will equal 1
-//       else {
-//         fs.unlink("./public/images/output/fjord_200_600.jpg", (err) => {
-//           if (err) console.log(err);
-//         });
-//         result = 1;
-//       }
-//       expect(result).toBe(1);
-//     });
-//   });
-// })
