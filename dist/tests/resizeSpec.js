202412dd2004deedd218fe6a3bc5af80df4a7c7e
d2gher <d2gher@gmail.com>
2022-08-25 13:06:30 +0200

    unit testing for image processing in resizing is done

--- dist/tests/resizeSpec.js
+++ dist/tests/resizeSpec.js
@@ -5,10 +5,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const supertest_1 = __importDefault(require("supertest"));
 const app_1 = __importDefault(require("../app"));
+const fs_1 = __importDefault(require("fs"));
 const request = (0, supertest_1.default)(app_1.default);
 describe("Server testing", () => {
     it("Test GET /resize", async () => {
         const res = await request.get("/resize");
         expect(res.statusCode).toBe(200);
+    });
+    it("Test image processing", async () => {
+        let result = 0;
+        // Creates a new image
+        await request.get("/resize?name=fjord&width=200&height=600");
+        // Searches if the image exists
+        fs_1.default.access("./public/images/output/fjord_200_600.jpg", async (err) => {
+            // if it didn't find it then that means the process failed, result will equal 0
+            if (err)
+                result = 0;
+            // if it didn find it then that means the process succeded
+            // the file will be removed and result will equal 1
+            else {
+                fs_1.default.unlink("./public/images/output/fjord_200_600.jpg", (err) => {
+                    if (err)
+                        console.log(err);
+                });
+                result = 1;
+            }
+            expect(result).toBe(1);
+        });
     });
 });
